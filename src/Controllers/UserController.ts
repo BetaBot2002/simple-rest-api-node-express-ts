cdc7efafdb2df21e5ea3e78d1b46d1047cf23722
Sinchan Nandy <105705266+BetaBot2002@users.noreply.github.com>
2024-08-27 02:01:48 +0530

    Registered User data sent back

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -4,9 +4,10 @@ import { IUsers, Users } from "../Models/Users.js";
 const registerUser = async (req: Request,res: Response)=>{
     const userData : IUsers=req.body
     const user = new Users(userData)
-    await user.save()
+    const data =await user.save()
     res.status(200).json({
-        message:"User Registered"
+        message:"User Registered",
+        user:data
     })
 }
 
